d901dc909d65daecea467d5697ed5483ead336dd
Cthulhux <>
2022-01-31 22:31:12 +0000

    Let's have a more serious license... +++ RELICENSED: CDDL 1.1 +++
    FossilOrigin-Name: 7f2a225f37966bd841eb2ec87af26d86f6426d2af22a1663ed7e20a2efe5630f

--- config.lisp
+++ config.lisp
@@ -1,8 +1,18 @@
 ;;; Web to RSS Parser
-;;; by tux. [ http://tuxproject.de ]
+;;; by tux0r [ https://tuxproject.de ]
 ;;;
-;;; Licensed under the terms of the WTFPL.
-;;; http://wtfpl.net/txt/copying/
+;;; The contents of this file are subject to the terms of the
+;;; Common Development and Distribution License, Version 1.1 only
+;;; (the "License").  You may not use this file except in compliance
+;;; with the License.
+;;;
+;;; See the file LICENSE in this distribution for details.
+;;; A copy of the CDDL is also available via the Internet at
+;;; https://spdx.org/licenses/CDDL-1.1.html
+;;;
+;;; When distributing Covered Code, include this CDDL HEADER in each
+;;; file and include the contents of the LICENSE file from this
+;;; distribution.
 
 
 (in-package #:rssparser)
